3c93a5a67ca8c2b85048b2621840493c22776f5d
Akhmad Fitriyan Ramadhan <akhmadfitriyanramadhan@gmail.com>
2023-09-30 11:19:43 +0700

    punya akhmad

---
+++ app/Http/Controllers/MasyarakatController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Masyarakat;
+use Illuminate\Http\Request;
+
+class MasyarakatController extends Controller
+{
+    public function index(){
+        // buat objek 
+        $pakdes = new Masyarakat();
+        //return view ('Masyarakat.index',['bebas'=>$pakdes->all()]);
+    }
+    public function registrasi(){
+        // buat objek 
+            return view('Masyarakat.registrasi');
+    }
+    public function data(Request $request){
+            // cek data yang dikirimkan
+            $tes = $request->validate([
+                'nik'=>'required|unique:masyarakat|max:16',
+                'nama'=>'required',
+                'username'=>'required|min:6',
+                'password'=>'required|min:4',
+                'telpon'=>'required|max:16'
+            ]);
+
+
+            $akhmad = new Masyarakat();
+            $akhmad->create([
+                'nik'=>$request->nik,
+                'nama'=>$request->nama,
+                'username'=>$request->username,
+                'password'=>$request->password,
+                'telpon'=>$request->telpon,
+            ]);
+            //return redirect('masyarakat/registrasi');
+            return back()->with('pesan','Selamat, anda sudah berhasil registrasi');
+    }
+    public function login(){
+        // buat objek
+        return view('Masyarakat.login');
+    }
+    public function ceklogin(Request $request){
+         $raden = new Masyarakat();
+         // cek username dan password exists (ada) di tabel masyarakat
+         if($raden->where('username',$request->input('username'))->where('password',$request->input('password'))->exists()){
+            return redirect('/');
+         }
+         return back()->with('pesan','username atau password salah');
+    }
+    public function laporan(){
+        // buat objek
+        return view('Masyarakat.laporan');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\MasyarakatController;
+use Illuminate\Routing\RouteRegistrar;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/',function(){
+    return view ('welcome');
+});
+
+// data masyrakat
+route::get('masyarakat',[MasyarakatController::class,'index']);
+
+
+
+// registrasi
+route::get('registrasi',[MasyarakatController::class,'registrasi']);
+route::post('data',[MasyarakatController::class,'data']);
+
+// login
+route::get('login',[MasyarakatController::class,'login']);
+route::post('login',[MasyarakatController::class,'ceklogin']);
+
+// laporan
+route::get('laporan',[MasyarakatController::class,'laporan']);
+route::post('laporan',[MasyarakatController::class,'laporan']);
